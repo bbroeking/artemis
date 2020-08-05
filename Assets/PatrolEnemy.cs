6e19ae9cd6ce011c13d4551676b9d6753d7be15d
Brian Broeking <bbroeking456@gmail.com>
2020-08-05 12:28:02 -0500

    AR-51: Basic Enemy Patrols

---
+++ Assets/PatrolEnemy.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Pathfinding;
+
+public class PatrolEnemy : MonoBehaviour
+{
+    [SerializeField] AIDestinationSetter aIDestination;
+    [SerializeField] private Transform[] patrolAnchors;
+    private Transform currentAnchor;
+    private int currentAnchorIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentAnchorIndex = 0;
+        currentAnchor = patrolAnchors[currentAnchorIndex];
+        aIDestination.target = currentAnchor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector2.Distance(this.transform.position, currentAnchor.transform.position);
+        Debug.Log(dist.ToString());
+        if (dist < 0.2){
+            currentAnchorIndex = (currentAnchorIndex + 1) % patrolAnchors.Length;
+            currentAnchor = patrolAnchors[currentAnchorIndex];
+            aIDestination.target = currentAnchor;
+        }
+    }
+}
