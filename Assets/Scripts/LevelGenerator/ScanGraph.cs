d71037f01905cc9b96130e4eae35f152620da335
Brian Broeking <bbroeking456@gmail.com>
2020-08-10 18:06:21 -0500

    AR-55: Create Graph on Zone

---
+++ Assets/Scripts/LevelGenerator/ScanGraph.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using Pathfinding;
+
+public class ScanGraph : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AstarPath.active.Scan();  
+    }
+}
